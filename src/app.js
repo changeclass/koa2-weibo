1aa1114373963ba43f6585c3f4fa6383fa04d116
XiaoKang <dreamytzk@outlook.com>
2020-12-15 19:53:41 +0800

    🎨 修改目录结构

--- src/app.js
+++ src/app.js
@@ -13,24 +13,20 @@ const users = require('./routes/users')
 onerror(app)
 
 // middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
+app.use(
+  bodyparser({
+    enableTypes: ['json', 'form', 'text']
+  })
+)
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
-app.use(views(__dirname + '/views', {
-  extension: 'ejs'
-}))
-
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+app.use(
+  views(__dirname + '/views', {
+    extension: 'ejs'
+  })
+)
 
 // routes
 app.use(index.routes(), index.allowedMethods())
@@ -39,6 +35,6 @@ app.use(users.routes(), users.allowedMethods())
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
-});
+})
 
 module.exports = app
