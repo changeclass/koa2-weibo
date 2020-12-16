b35daa3c0b1ac83d30954ca87f39627b0af531dc
XiaoKang <dreamytzk@outlook.com>
2020-12-16 15:56:42 +0800

    ✨ 连接池

--- sequelize-test/src/seq.js
+++ sequelize-test/src/seq.js
@@ -4,6 +4,13 @@ const conf = {
   host: 'localhost',
   dialect: 'mysql'
 }
+
+// 连接池
+// conf.pool = {
+//   max: 5,
+//   min: 0,
+//   idle: 10000
+// }
 // 数据库名、账户名、密码
 const seq = new Sequelize('koa2_weibo', 'root', 'root', conf)
 
