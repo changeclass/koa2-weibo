1994c409ca7a0bbaba3dc661278a0d5556198def
XiaoKang <dreamytzk@outlook.com>
2020-12-17 19:21:08 +0800

    ✨ 已登录状态

--- src/routes/view/users.js
+++ src/routes/view/users.js
@@ -4,12 +4,32 @@
  */
 const router = require('koa-router')()
 
+/**
+ * @author: 小康
+ * @url: https://xiaokang.me
+ * @param {Object} ctx
+ * @description: 判断是否登录
+ */
+function getLoginInfo(ctx) {
+  let data = {
+    isLogin: false
+  }
+  const userInfo = ctx.session.userInfo
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName
+    }
+  }
+  return data
+}
+
 router.get('/login', async function (ctx, next) {
-  await ctx.render('login', {})
+  await ctx.render('login', getLoginInfo(ctx))
 })
 
 router.get('/register', async function (ctx, next) {
-  await ctx.render('register', {})
+  await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router
