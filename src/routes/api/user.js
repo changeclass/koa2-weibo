4a15021878a627fe6b9759a9d569ccb930c03046
XiaoKang <dreamytzk@outlook.com>
2020-12-20 10:12:30 +0800

    ✨ 获取关注用户列表接口

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -22,6 +22,7 @@ const userValidate = require('../../validator/user')
 const { genValidator } = require('../../middlewares/validator')
 const { isTest } = require('../../utils/env')
 const { loginCheck } = require('../../middlewares/loginChecks')
+const { getFollowers } = require('../../controller/user-relation')
 router.prefix('/api/user')
 
 // 注册路由
@@ -76,5 +77,16 @@ router.patch(
 // 退出登录
 router.post('/logout', loginCheck, async (ctx, next) => {
   ctx.body = await logout(ctx)
+})
+
+// 获取 at 列表
+router.get('/getAtList', loginCheck, async (ctx, next) => {
+  const { id: userId } = ctx.session.userInfo
+  const result = await getFollowers(userId)
+  const { followersList } = result.data
+  const list = followersList.map((user) => {
+    return `${user.nickName}-${user.userName}`
+  })
+  ctx.body = list
 })
 module.exports = router
